ebd11139c77b46deb598f9204cb6cd48577269a1
abfloresc <abfloresc@unjbg.edu.pe>
2024-07-08 08:04:45 -0500

    eje1.cpp

--- Desktop/Nueva carpeta/eje1.cpp
+++ Desktop/Nueva carpeta/eje1.cpp
@@ -3,20 +3,20 @@
 using namespace std;
 int main() {
     int n;
-    cout << "Ingrese la cantidad de numeros que desea almacenar: ";cin>>n;
+    cout<<"Ingrese la cantidad de numeros que desea almacenar: ";cin>>n;
     vector<int> numeros(n);
-    int r = 0;
-    int contadorCeros = 0;
-    for (int i = 0; i < n; ++i) {
+    int r=0;
+    int contadorCeros=0;
+    for (int i=0; i<n;++i) {
         cout<<"Ingrese el numero "<<i + 1<<": ";
-        cin>> numeros[i];
-        r += numeros[i];
-        if (numeros[i] == 0) {
-            contadorCeros++;
+        cin>>num[i];
+        r +=num[i];
+        if (num[i] == 0) {
+            ceros++;
         }
     }
-    double porcentajeCeros = (static_cast<double>(contadorCeros) / n) * 100;
+    double pceros=(static_cast<double>(ceros)/n)*100;
     cout<<"La sumatoria de los numeros ingresados es: "<<r<<endl;
-    cout<<"El porcentaje de ceros ingresados es: "<<porcentajeCeros<<"%"<<endl;
+    cout<<"El porcentaje de ceros ingresados es: "<<pceros<<"%"<<endl;
     return 0;
 }
