a051f6ebae4f14399ef677d25ee23aec422730eb
abfloresc <abfloresc@unjbg.edu.pe>
2024-07-08 08:47:16 -0500

    eje2.cpp

--- Desktop/Nueva carpeta/problema 2.cpp
+++ Desktop/Nueva carpeta/problema 2.cpp
@@ -1,36 +1,34 @@
 #include <iostream>
 using namespace std;
-
-int main() {
+int main(){
     int n;
-    cout << "Ingrese la cantidad de elementos: ";
-    cin >> n;
+    cout<<"Ingrese la cantidad de elementos: ";
+    cin>>n;
     int arreglo[n];
     int pares[n], impares[n];
-    int numPares = 0, numImpares = 0;
+    int npares=0, nimpares=0;
     cout << "Ingrese los elementos del arreglo:\n";
-    for (int i = 0; i < n; ++i) {
-        cin >> arreglo[i];
+    for (int i=0;i<n; ++i) {
+        cin>>arreglo[i];
     }
-    for (int i = 0; i < n; ++i) {
-        if (arreglo[i] % 2 == 0) {
-            pares[numPares] = arreglo[i];
-            numPares++;
+    for(int i=0;i<n; ++i) {
+        if (arreglo[i] % 2==0) {
+            pares[npares]=arreglo[i];
+            npares++;
         } else {
-            impares[numImpares] = arreglo[i];
-            numImpares++;
+            impares[nimpares] = arreglo[i];
+            nimpares++;
         }
     }
-    cout << "Arreglo de elementos pares:\n";
-    for (int i = 0; i < numPares; ++i) {
-        cout << pares[i] << " ";
+    cout<<"Arreglo de elementos pares:\n";
+    for(int i = 0; i<npares; ++i) {
+        cout<<pares[i]<< " ";
     }
-    cout << endl;
-    cout << "Arreglo de elementos impares:\n";
-    for (int i = 0; i < numImpares; ++i) {
-        cout << impares[i] << " ";
+    cout<<endl;
+    cout<<"Arreglo de elementos impares:\n";
+    for(int i=0;i<nimpares;++i) {
+        cout<<impares[i] << " ";
     }
-    cout << endl;
-
+    cout<<endl;
     return 0;
 }
